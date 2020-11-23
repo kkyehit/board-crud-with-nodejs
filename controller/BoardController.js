10838d13693c888562a1691e6dde630839c833ff
unknown <Administrator@SKCC18N00002.SKCC.NET>
2020-11-23 17:33:43 +0900

    swagger-ui 작성을 위한 코드 작성

--- controller/BoardController.js
+++ controller/BoardController.js
@@ -1,3 +1,55 @@
+/**
+ * @swagger
+ * tags:
+ *   name: Board
+ *   description: 게시판 CRUD
+ */
+ 
+/**
+ * @swagger
+ * /boards:
+ *   get:
+ *     summary: 사용자 정보 가져오기
+ *     description: "사용자 정보 리스트"
+ *     operationId: "getBoardList"
+ *     produces:
+ *       - "application/json"
+ *     tags: [boards]
+ *     parameters:
+ *       - in: query
+ *         name: pageNum
+ *         type: string
+ *         description: 페이지 번호
+ *         required: true
+ *     responses:
+ *       "200":
+ *          description: "successful operation"
+ *          schema : 
+ *            type : "array"
+ *            items :
+ *              $ref: "#/definitions/Board"
+ *       "400":
+ *          description: "Invaild"
+ */
+
+ /**
+  * @swagger
+  * definitions:
+  *   Board:
+  *     tpye: "object"
+  *     properties:
+  *       id:
+  *         type: "integer"
+  *         format: "int64"
+  *       title:
+  *         type: "string"
+  *       author_name:
+  *         type: "string"
+  *       content:
+  *         type: "string"
+  *       createdAt:
+  *         type: "datetime"
+  */
 const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
