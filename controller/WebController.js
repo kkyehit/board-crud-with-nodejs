78a2b906d01fc0534b19b4409cd15b0815f9a4d1
unknown <Administrator@SKCC18N00002.SKCC.NET>
2020-11-24 10:16:05 +0900

    게시글 작성 요청에 대한 controller

--- controller/WebController.js
+++ controller/WebController.js
@@ -10,6 +10,6 @@ const webService = require('../service/WebService')
 router.get("/", webService.index);
 router.get('/board', webService.inputBoard);
 
-
+router.post('/board.do', webService.boardDo);
 
 module.exports = router;
