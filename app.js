c77fd83bec04408dc5b82602f47977e59bdbd663
unknown <Administrator@SKCC18N00002.SKCC.NET>
2020-11-24 12:50:43 +0900

    ejs를 사용하기 위한 설정 추가

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const swaggerUi = require('swagger-ui-express');
 const express = require('express');
 const app = express();
 
+/* EJS 사용 */
+const ejs = require('ejs');
+/* view engine으로 ejs를 사용한다. */
+app.set('view engine', 'ejs');
+/* template 폴더를 ./webapp으로 설정한다. */
+app.set('views', './webapp');
+
 app.use("/boards",require("./controller/BoardController"));
 app.use("/api-docs",swaggerUi.serve, swaggerUi.setup(require("./config/SwaggerUiConfig").swaggerSpec ))
 app.use("/web",require("./controller/WebController"));
