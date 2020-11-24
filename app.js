8f429fea22024253fddd3e3daefc154cc412be48
unknown <Administrator@SKCC18N00002.SKCC.NET>
2020-11-24 09:45:06 +0900

    /web 요청에 대한 미들웨어 추가

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 
 app.use("/boards",require("./controller/BoardController"));
 app.use("/api-docs",swaggerUi.serve, swaggerUi.setup(require("./config/SwaggerUiConfig").swaggerSpec ))
+app.use("/web",require("./controller/WebController"));
 
 app.listen(8080, () => {
     console.log("board crud server started");
