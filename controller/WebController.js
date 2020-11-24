d89062e803af80b474418fec3749bdb993682545
unknown <Administrator@SKCC18N00002.SKCC.NET>
2020-11-24 10:14:35 +0900

    form으로 전송되는 데이터를 얻기 위한 body-parser 설정

--- controller/WebController.js
+++ controller/WebController.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const bodyParser = require('body-parser');
+
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({extended: true}));
+
 const webService = require('../service/WebService')
 
 router.get("/", webService.index);
