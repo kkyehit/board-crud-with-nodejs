598105aab326ad863a124f9406a6ffb9dda79242
unknown <Administrator@SKCC18N00002.SKCC.NET>
2020-11-24 10:26:02 +0900

    web을 통해 작성된 게시글을 DB에 등록하는 기능 구현

--- service/WebService.js
+++ service/WebService.js
@@ -14,4 +14,11 @@ exports.inputBoard = (req, res) => {
     res.writeHead(200).end(fs.readFileSync(__dirname + baseUrl + html));
 }
 
+exports.boardDo = (req, res) => {
+    const model = {
+        title: req.body.title,
+        author_name: req.body.author_name,
+        content: req.body.content
+    };
+    boardRepository.createBoard(model).then(()=>{this.index(req, res);});
 }
